ae99dc7f35e9a280a9f14761cddfebea9ea9acce
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-08-10 07:09:13 +0700

    Get the data from form

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,1 +1,52 @@
+import 'package:flutter/material.dart';
 
+class AddToDoPage extends StatefulWidget {
+  const AddToDoPage({super.key});
+
+  @override
+  State<AddToDoPage> createState() => _AddToDoPageState();
+}
+
+class _AddToDoPageState extends State<AddToDoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add ToDo'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Submit'),
+          )
+        ],
+      ),
+    );
+  }
+
+  void submitData() {
+    // Get the data from form
+    final tile = titleController.text;
+    final description = descriptionController.text;
+    // Submit data to the server
+    // Show success or fail message based on status
+  }
+}
--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,3 +1,4 @@
+import 'package:app_todo_xd/screens/add_page.dart';
 import 'package:flutter/material.dart';
 
 class ToDoListPage extends StatefulWidget {
@@ -14,6 +15,17 @@ class _ToDoListPageState extends State<ToDoListPage> {
       appBar: AppBar(
         title: Text('Todo List'),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAddPage,
+        label: Text('Add Todo'),
+      ),
     );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => AddToDoPage(),
+    );
+    Navigator.push(context, route);
   }
 }
