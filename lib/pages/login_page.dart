2c4b66b2fdb4aca7bac193cfc840a6838c9f0859
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-08-18 14:11:37 +0700

    Login UI final

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_todo_xd/components/my_button.dart';
+import 'package:app_todo_xd/components/square.tile.dart';
 import 'package:flutter/material.dart';
 import 'package:app_todo_xd/components/my_textfield.dart';
 
@@ -15,81 +16,142 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 50),
+              // logo
+              const Icon(
+                Icons.lock,
+                size: 100,
+                color: Colors.black,
+              ),
 
-                // logo
-                const Icon(
-                  Icons.lock,
-                  size: 100,
-                  color: Colors.black,
+              const SizedBox(height: 50),
+
+              // Welcome back, you've been missed
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
                 ),
+              ),
 
-                const SizedBox(height: 50),
+              const SizedBox(height: 25),
 
-                // Welcome back, you've been missed
-                Text(
-                  'Welcome back you\'ve been missed!',
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  ),
+              // usename textField
+              MyTextfield(
+                controller: usernameController,
+                hinText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 10),
+
+              // Password textfield
+              MyTextfield(
+                controller: passwordController,
+                hinText: 'Password',
+                obscureText: true,
+              ),
+
+              const SizedBox(height: 10),
+
+              // forgot password?
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600]),
+                    ),
+                  ],
                 ),
+              ),
 
-                const SizedBox(height: 25),
+              const SizedBox(height: 25),
 
-                // usename textField
-                MyTextfield(
-                  controller: usernameController,
-                  hinText: 'Username',
-                  obscureText: false,
-                ),
+              // sign in buttons
+              MyButton(
+                onTap: signUserIn,
+              ),
 
-                const SizedBox(height: 10),
+              const SizedBox(height: 50),
 
-                // Password textfield
-                MyTextfield(
-                  controller: passwordController,
-                  hinText: 'Password',
-                  obscureText: true,
-                ),
-
-                const SizedBox(height: 10),
-
-                // forgot password?
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Forgot Password?',
-                        style: TextStyle(color: Colors.grey[600]),
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
                       ),
-                    ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(height: 50),
+
+              // google + apple sign in buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  // google button
+                  SquareTile(imagePath: 'lib/images/google.png'),
+
+                  SizedBox(width: 25),
+
+                  // apple button
+                  SquareTile(imagePath: 'lib/images/apple.png'),
+                ],
+              ),
+
+              const SizedBox(height: 30),
+
+              // not a member ? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
                   ),
-                ),
-
-                const SizedBox(height: 25),
-
-                // sign in buttons
-                MyButton(
-                  onTap: signUserIn,
-                ),
-
-                const SizedBox(height: 25),
-                // or continue with
-
-                // google + apple sign in buttons
-
-                // not a member ? register now
-              ],
-            ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
